62beaa9c4666e630e9d945f7035b9a86f0028031
turana1998 <turana221298@gmail.com>
2021-12-08 21:56:59 +0300

    add

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+    console.log("kk")
+  }
+  
+  function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+    console.log("kk")
+  }
+  
