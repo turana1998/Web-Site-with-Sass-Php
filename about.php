c65ed40ec9a47575f10a42930e5f3d8a78925cd7
turana1998 <turana221298@gmail.com>
2021-12-27 15:58:27 +0300

    add

--- about.php
+++ about.php
@@ -19,7 +19,7 @@
             <div class="col-12 col-lg-6">
                 <img class="about_img" src="./img/media.png" />
             </div>
-            <div class="col-12 col-lg-6 px-5">
+            <div class="col-12 col-lg-6 px-xl-5">
                 <h4>Arash Dental&Beauty</h4>
                 <p>Arash şirkəti, 1994-cü ildə təsis edilmiş və hazırda Azərbaycanda stomatoloji, kosmetoloji avadanlıq
                     və məhsulların satışında liderlik mövqeyindədir.
@@ -33,7 +33,7 @@
                     yalnız yüksək keyfiyyətli və orijinal məhsullar təklif edir.
                 </p>
             </div>
-            <div class="col-12 col-lg-6 px-5">
+            <div class="col-12 col-lg-6 px-xl-5">
                 <h4>Arash Dental&Beauty</h4>
                 <p>Arash şirkəti, 1994-cü ildə təsis edilmiş və hazırda Azərbaycanda stomatoloji, kosmetoloji avadanlıq
                     və məhsulların satışında liderlik mövqeyindədir.
