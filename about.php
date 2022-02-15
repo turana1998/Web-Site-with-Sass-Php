2d4552e2c543079a773ce90d1bbab1657012011a
turana1998 <turana221298@gmail.com>
2022-02-15 16:17:49 +0300

    information

--- about.php
+++ about.php
@@ -35,17 +35,41 @@
             </div>
             <div class="col-12 col-lg-6 px-xl-5">
                 <h4>Arash Dental&Beauty</h4>
-                <p>Arash şirkəti, 1994-cü ildə təsis edilmiş və hazırda Azərbaycanda stomatoloji, kosmetoloji avadanlıq
-                    və məhsulların satışında liderlik mövqeyindədir.
-                    <br>
-                    <br>
-                    Şirkətimiz, Candela, Ionto Comed, Septodont, İBSA, Medytox və sairə dünyanın aparıcı istehsalçıları
-                    ilə əməkdaşlıq edir.
-                    <br>
-                    <br>
-                    Ümumilikdə 40 -dək məşhur brendlərin məhsullarının satışı ilə məşğul olan şirkətimiz,müştərilərinə
-                    yalnız yüksək keyfiyyətli və orijinal məhsullar təklif edir.
+                <p>Bütün mütəxəssislər, mütəmadi olaraq, beynəlxalq seminar,təlim və kurslarda iştirak etməklə öz
+                    kvalifikasiyalarını artırırlar.
+
+                    Araş şirkəti, periodik olaraq, müxtəlif təlim və əyləncəli tədbirlər, təqdimatlar, seminarlar
+                    keçirir.
+
+                    Tədbirlərin aparıcısı və təlimçisi qismində, öz təcrübələri ilə bölüşən, qabaqcıl müalicə metodları
+                    haqqında danışan, masster-klasslar keçirən, xarici partnyorlarımızın nümayəndələri dəvət olunurlar.
                 </p>
+            </div>
+            <div class="col-12 col-lg-6">
+                <img class="about_img" src="./img/media.png" />
+            </div>
+            <div class="col-12 col-lg-6">
+                <img class="about_img" src="./img/media.png" />
+            </div>
+            <div class="col-12 col-lg-6 px-xl-5">
+                <h4>Avadanlıqların servis xidməti</h4>
+                <p>Satışa çıxarılan bütün avadanlıqlar rəsmi zəmanət ilə təmin olunur və avadanlığın texniki xidməti
+                    üçün zəruri olan sənədlər təqdim edilir.
+
+                    Lazım gələrsə ,müştərilərimizin işçiləri üçün təlimlər keçiririk.
+
+                    24/7 rejimində fəaliyyət göstərən servis mərkəzimizdə, mütəxəssislərimiz,peşəkar təmir və
+                    avadanlıqlarda texniki xidmət göstərir.
+                </p>
+            </div>
+            <div class="col-12 col-lg-6 px-xl-5">
+
+                <p>Bütün aparılan işlərdə, servis zəmanəti təqdim edilir və təmir edilərkən də yalnız orijinal ehtiyat
+                    hissələrindən istifadə edilir.
+
+                    Arash Dental and Beauty - stomatoloji və kosmetoloji bazarda - 25 illik qüsursuz fəaliyyət.
+
+                    Bizimlə əməkdaşlıq ,uğurunuzun təminatıdır.</p>
             </div>
             <div class="col-12 col-lg-6">
                 <img class="about_img" src="./img/media.png" />
--- service.php
+++ service.php
@@ -25,7 +25,8 @@
                             Sifarişlərin çatdırılması Azərbaycanın bütün bölgələrində həyata keçirilir.
 
 
-                            Çatdırılma kuryer xidməti tərəfindən həyata keçirildiyi üçün, məhsulların təhlükəsizliyinə və
+                            Çatdırılma kuryer xidməti tərəfindən həyata keçirildiyi üçün, məhsulların təhlükəsizliyinə
+                            və
                             bütövlüyünə görə cavabdehlik daşıyırıq.
 
 
@@ -42,12 +43,16 @@
                         <h3>Çatdırılma müddəti</h3>
                         <table>
                             <tr>
-                                <td> <p>Bazar ertəsi – Şənbə </p></td>
+                                <td>
+                                    <p>Bazar ertəsi – Şənbə </p>
+                                </td>
                                 <td><span>9:00 – 19:00</span></td>
 
                             </tr>
                             <tr>
-                                <td> <p> Bazar günü </p></td>
+                                <td>
+                                    <p> Bazar günü </p>
+                                </td>
                                 <td><span>10:00 – 19:00</span></td>
 
                             </tr>
@@ -59,7 +64,8 @@
                             Bakı şəhəri ərazisində çatdırılma müddəti 2-3 saat.
 
 
-                            Azərbaycan üzrə çatdırılma şərtləri və müddəti telefon danışığı vasitəsilə, menecerlə individual
+                            Azərbaycan üzrə çatdırılma şərtləri və müddəti telefon danışığı vasitəsilə, menecerlə
+                            individual
                             olaraq keçirilir.
                         </p>
                     </div>
@@ -73,39 +79,49 @@
                 <div class="delivery_">
                     <img src="./img/s1.png" />
                     <div class="">
-                        <h3>Lorem İpsum</h3>
+                        <h3>Müştəri xidməti</h3>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa amet diam ultrices nec
-                            feugiat justo. Laoreet iaculis adipiscing egestas lorem. Et euismod tristique ut diam a,
-                            ultricies nullam ultrices vulputate. Aenean id ultricies purus quam ut. Convallis odio
-                            commodo lorem at a non aliquet tellus tellus. Gravida enim tristique egestas in netus.
-
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa amet diam ultrices nec
-                            feugiat justo. Laoreet iaculis adipiscing egestas lorem. Et euismod tristique ut diam a,
-                            ultricies nullam ultrices vulputate. Aenean id ultricies purus quam ut. Convallis odio
-                            commodo lorem at a non aliquet tellus tellus. Gravida enim tristique egestas in netus.
-                        </p>
+                            Müştərilərimizi yüksək səviyyədə xidmətlə təmin etmək məqsədilə, onlara 24×7 rejimində
+                            servis xidməti göstəririk</p>
                     </div>
                 </div>
             </div>
             <div class="col-12 ">
                 <div class="delivery_">
+                    <img src="./img/d3.png" />
+                    <div class="">
+
+                        <h3> Çatdırılma xidməti</h3>
+                        <p>
+                           
+                            Arash şirkətindən aldığınız hər bir avadanlıq müəyyən vaxta və qaydalara əsasən sizə
+                            çatdırılır. Çatdırılma xidməti həftənin bütün günləri fəaliyyət göstərir. "Arash Servis"
+                            əməkdaşları müştərilərlə əlaqə saxlayaraq uyğun zaman ərzində avadanlığı ünvana çatdırırlar.
+                            Alınan avadanlığın qəbulu alıcıların iştirakı ilə tam yoxlanılaraq həyata keçirilir.
+                            Avadanlıqların Bakı şəhəri daxili çatdırılması pulsuzdur.
+                            Avadanlıq müştəri tərəfindən əldə edildikdən sonra, Araş şirkətinin servis mərkəzi zəmanət
+                            talonunun düzgün və tam doldurulduğu halda 1 illik pulsuz zəmanət xidməti göstərir. Zəmanət
+                            talonunda satış tarixi, avadanlığın adı, seriya nömrəsi göstərilməli, “Araş" şirkətinin
+                            nümayəndəsinin imzası olmalıdır. Həmçinin təhvil-təslim blankı tərtib edilir.
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 ">
+                <div class="delivery_">
                     <img src="./img/s2.png" />
                     <div class="">
 
-                        <h3>Lorem İpsum</h3>
+                        <h3>Servis xidməti</h3>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa amet diam ultrices nec
-                            feugiat justo. Laoreet iaculis adipiscing egestas lorem. Et euismod tristique ut diam a,
-                            ultricies nullam ultrices vulputate. Aenean id ultricies purus quam ut. Convallis odio
-                            commodo lorem at a non aliquet tellus tellus. Gravida enim tristique egestas in netus.
+                            SERVİS MƏRKƏZİMİZDƏ GÖSTƏRİLƏN XİDMƏTLƏR:<br>
+                            1. Avadanlığın çatdırılması və quraşdırılması<br>
+                            2. Avadanlığın istifadəsinin öyrədilməsi (təlim)<br>
+                            3. Avadanlığın təmiri<br>
+                            4. Avadanlığın montajı demontajı<br>
+                            5. 24 saat servis xidməti </p>
 
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa amet diam ultrices nec
-                            feugiat justo. Laoreet iaculis adipiscing egestas lorem. Et euismod tristique ut diam a,
-                            ultricies nullam ultrices vulputate. Aenean id ultricies purus quam ut. Convallis odio
-                            commodo lorem at a non aliquet tellus tellus. Gravida enim tristique egestas in netus.
-                        </p>
-                       
                     </div>
                 </div>
             </div>
@@ -114,18 +130,18 @@
         </div>
         <div class="row justify-content-center mt-5 mb-5 align-items-center">
             <div class="col-4">
-                <img class="w-100" src="./img/img1.png"/>
+                <img class="w-100" src="./img/img1.png" />
             </div>
             <div class="col-6">
                 <div class="row align-items-end">
                     <div class="col-6">
-                        <img class="w-100" src="./img/img2.png"/>
+                        <img class="w-100" src="./img/img2.png" />
                     </div>
                     <div class="col-6">
-                        <img class="w-100" src="./img/img3.png"/>
+                        <img class="w-100" src="./img/img3.png" />
                     </div>
                     <div class="col-10 mt-3">
-                            <img class="w-100" src="./img/img4.png"/>
+                        <img class="w-100" src="./img/img4.png" />
                     </div>
                 </div>
             </div>
