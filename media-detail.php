5e77cf09f52685b9bd13b78f388a1a69a2f6a223
turana1998 <turana221298@gmail.com>
2022-02-24 20:49:34 +0300

    news detail side design

--- media-detail.php
+++ media-detail.php
@@ -20,15 +20,15 @@
             <h3>Lazer epilyasiyasının qızıl standartı
                 nə deməkdir?</h3>
             <span>10.11.2021</span>
-            
+
         </div>
         <div class="row">
             <div class="col-12 col-lg-7">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit mattis nisi, viverra ultrices
-                lacus. Nunc auctor diam mollis justo aliquet ultricies. Aliquam neque dui, egestas id mauris non,
-                luctus
-                blandit neque. Pellentesque posuere risus. Proin commodo dui ut augue semper condimentum.<br>
-            </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit mattis nisi, viverra ultrices
+                    lacus. Nunc auctor diam mollis justo aliquet ultricies. Aliquam neque dui, egestas id mauris non,
+                    luctus
+                    blandit neque. Pellentesque posuere risus. Proin commodo dui ut augue semper condimentum.<br>
+                </p>
                 <img class="news_img" src="./img/media.png" />
                 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit mattis nisi, viverra ultrices
                     lacus. Nunc auctor diam mollis justo aliquet ultricies. Aliquam neque dui, egestas id mauris non,
@@ -39,56 +39,31 @@
 
                 <div class="news_detail d-flex">
 
-                    <img width="200px" height="150px" src="./img/news.png" />
-
-                    <div class="text_">
-                        <h4>Lazer epilyasiyasının qızıl standartı nə deməkdir?</h4>
-                        <p>Lazer, stomotoloji və kosmetoloji məhsulların satışı və servisi...</p>
-                        <div class="d-flex justify-content-between ">
-                            <div class="time">12.09.21</div>
-                            <a href="media-detail.php"><svg width="41" height="12" viewBox="0 0 41 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M0 5.87143H39.6M39.6 5.87143L33.8049 1M39.6 5.87143L33.8049 10.7429" stroke="#000"/>
-</svg>
-</a>
+                    <div class="row">
+                        <div class="col-6">
+                            <img width="100%" height="100%" src="./img/news.png" />
                         </div>
 
-                    </div>
-                </div>
-                <div class="news_detail d-flex">
+                        <div class="col-6">
+                            <div class="text_">
+                                <h4>Lazer epilyasiyasının qızıl standartı nə deməkdir?</h4>
+                                <p>Lazer, stomotoloji və kosmetoloji məhsulların satışı və servisi...</p>
+                                <div class="d-flex justify-content-between ">
+                                    <div class="time">12.09.21</div>
+                                    <a href="media-detail.php"><svg width="41" height="12" viewBox="0 0 41 12"
+                                            fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M0 5.87143H39.6M39.6 5.87143L33.8049 1M39.6 5.87143L33.8049 10.7429"
+                                                stroke="#000" />
+                                        </svg>
+                                    </a>
+                                </div>
 
-                    <img width="200px" height="150px" src="./img/news.png" />
-
-                    <div class="text_">
-                        <h4>Lazer epilyasiyasının qızıl standartı nə deməkdir?</h4>
-                        <p>Lazer, stomotoloji və kosmetoloji məhsulların satışı və servisi...</p>
-                        <div class="d-flex justify-content-between ">
-                            <div class="time">12.09.21</div>
-                            <a href="media-detail.php"><svg width="41" height="12" viewBox="0 0 41 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M0 5.87143H39.6M39.6 5.87143L33.8049 1M39.6 5.87143L33.8049 10.7429" stroke="#000"/>
-</svg>
-</a>
+                            </div>
                         </div>
-
                     </div>
                 </div>
-                <div class="news_detail d-flex">
-
-                    <img width="200px" height="150px" src="./img/news.png" />
-
-                    <div class="text_">
-                        <h4>Lazer epilyasiyasının qızıl standartı nə deməkdir?</h4>
-                        <p>Lazer, stomotoloji və kosmetoloji məhsulların satışı və servisi...</p>
-                        <div class="d-flex justify-content-between ">
-                            <div class="time">12.09.21</div>
-                            <a href="media-detail.php"><svg width="41" height="12" viewBox="0 0 41 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M0 5.87143H39.6M39.6 5.87143L33.8049 1M39.6 5.87143L33.8049 10.7429" stroke="#000"/>
-</svg>
-</a>
-                        </div>
-
-                    </div>
-                </div>
-
+               
 
 
             </div>
